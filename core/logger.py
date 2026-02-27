db78c46d0e9c3c57454b7b1bb8a27bf5c2410cd2
dd-jiyun <dlwldbs8015@gmail.com>
2026-02-27 17:34:37 +0900

    refactor: TimedRotatingFileHandler로 개선

--- core/logger.py
+++ core/logger.py
@@ -1,34 +1,47 @@
-import logging
 import os
+import logging
 
-# 로그 파일이 저장될 디렉토리 (없으면 자동 생성)
-LOG_DIR = 'data' 
-os.makedirs(LOG_DIR, exist_ok=True)
+from core.config import LOG_DIR
+from logging.handlers import TimedRotatingFileHandler
 
-def _setup_logger():
+def setup_logger():
     logger = logging.getLogger("DEKK_Crawler")
     
-    if logger.hasHandlers():
+    if logger.handlers:
         return logger
 
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-    all_handler = logging.FileHandler(f'{LOG_DIR}/crawler.log', encoding='utf-8')
-    all_handler.setFormatter(formatter)
-    all_handler.setLevel(logging.INFO)
-    logger.addHandler(all_handler)
-
-    err_handler = logging.FileHandler(f'{LOG_DIR}/error.log', encoding='utf-8')
-    err_handler.setFormatter(formatter)
-    err_handler.setLevel(logging.ERROR)
-    logger.addHandler(err_handler)
-    
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
-    console_handler.setLevel(logging.INFO)
-    logger.addHandler(console_handler)
 
+    file_handler = TimedRotatingFileHandler(
+        filename=os.path.join(LOG_DIR, "crawler.log"),
+        when="midnight",
+        interval=1,
+        backupCount=30,   
+        encoding="utf-8"
+    )
+    file_handler.suffix = "%Y-%m-%d"
+    file_handler.setFormatter(formatter)
+
+    # 에러 전용
+    err_handler = TimedRotatingFileHandler(
+        filename=os.path.join(LOG_DIR, "error.log"),
+        when="midnight",
+        interval=1,
+        backupCount=30,
+        encoding="utf-8"
+    )
+    err_handler.suffix = "%Y-%m-%d"
+    err_handler.setFormatter(formatter)
+    err_handler.setLevel(logging.ERROR)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    logger.addHandler(err_handler)
+    
     return logger
 
-logger = _setup_logger()
+logger = setup_logger()
